b4206e98c8210356dd24e29a0c84371b3eed31c8
Al-AminWeb <143194944+Al-AminWeb@users.noreply.github.com>
2025-05-01 20:47:45 +0600

    implement marquee & design navbar

---
+++ src/components/Navbar.jsx
@@ -0,0 +1,24 @@
+import React from 'react';
+import {NavLink} from "react-router";
+import user from '../assets/user.png'
+
+const Navbar = () => {
+    return (
+        <div className='flex justify-between items-center'>
+          <div className=''></div>
+            <div className="flex gap-3 text-accent">
+                <NavLink to='/'>
+                    Home
+                </NavLink>
+                <NavLink to='/about'>About</NavLink>
+                <NavLink to='/career'>career</NavLink>
+            </div>
+            <div className="flex gap-3">
+                <img src={user} alt=""/>
+                <button className='btn btn-primary px-10'>Login</button>
+            </div>
+        </div>
+    );
+};
+
+export default Navbar;
--- src/layouts/HomeLayout.jsx
+++ src/layouts/HomeLayout.jsx
@@ -1,12 +1,20 @@
 import React from 'react';
 import {Outlet} from "react-router";
 import Header from "../components/Header.jsx";
+import LatestNews from "../components/LatestNews.jsx";
+import Navbar from "../components/Navbar.jsx";
 
 const HomeLayout = () => {
     return (
         <div>
             <header>
                 <Header />
+                <section className='w-11/12 mx-auto my-3 '>
+                    <LatestNews />
+                </section>
+                <nav className='w-11/12 mx-auto my-3 '>
+                    <Navbar/>
+                </nav>
             </header>
             <main>
 
