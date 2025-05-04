2bd14fad6e0b8276c38b5190616edf93342194be
Al-AminWeb <143194944+Al-AminWeb@users.noreply.github.com>
2025-05-05 02:12:04 +0600

    auth context added

---
+++ src/firebase/firebase.config.js
@@ -0,0 +1,18 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+    apiKey: "AIzaSyCT5Yc3DmbVY3SsBrOWKkquzzrxEEzqWxg",
+    authDomain: "dragon-news-breaking-861c4.firebaseapp.com",
+    projectId: "dragon-news-breaking-861c4",
+    storageBucket: "dragon-news-breaking-861c4.firebasestorage.app",
+    messagingSenderId: "484251443022",
+    appId: "1:484251443022:web:97f4608da82dde1f3e081f"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+export default app;
---
+++ src/provider/AuthProvider.jsx
@@ -0,0 +1,24 @@
+import React, {createContext, useState} from 'react';
+
+export const AuthContext = createContext();
+
+const AuthProvider = ({children}) => {
+
+    const [user, setUser] = useState({
+        name:"alamin",
+        email:"alamin@gmail.com"
+    });
+
+    const authData ={
+        user,
+        setUser
+    }
+
+    return (
+        <AuthContext value={authData}>
+            
+        </AuthContext>
+    );
+};
+
+export default AuthProvider;
